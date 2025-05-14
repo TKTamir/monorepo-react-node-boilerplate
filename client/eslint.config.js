b4aec5c5f23e6ddbaf359a42dd4e211b8fccb8d9
tamir <tamirkhy@gmail.com>
2025-05-14 18:14:47 +0300

    Adjust client eslintconfig

--- client/eslint.config.js
+++ client/eslint.config.js
@@ -3,31 +3,31 @@ import globals from "globals";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
 
-export default {
+export default [
   ...baseConfig,
-  ignores: [...baseConfig.ignores, ".vite"],
-  files: ["**/*.{ts,tsx}"],
-  languageOptions: {
-    ...baseConfig.languageOptions,
-    globals: { ...globals.browser, ...globals.node },
-    project: "./tsconfig.json",
-  },
-  plugins: {
-    ...baseConfig.plugins,
-    "react-hooks": reactHooks,
-    "react-refresh": reactRefresh,
-  },
-  extends: [...baseConfig.extends, reactHooks.configs.recommended],
-  rules: {
-    "react-refresh/only-export-components": [
-      "warn",
-      { allowConstantExport: true },
-    ],
-    "react/prop-types": "off",
-  },
-  settings: {
-    react: {
-      version: "detect",
+  {
+    ignores: ["dist", "node_modules", ".vite"],
+    files: ["**/*.{ts,tsx}"],
+    languageOptions: {
+      globals: { ...globals.browser, ...globals.node },
+    },
+    plugins: {
+      "react-hooks": reactHooks,
+      "react-refresh": reactRefresh,
+    },
+    rules: {
+      "react-hooks/rules-of-hooks": "error",
+      "react-hooks/exhaustive-deps": "warn",
+      "react-refresh/only-export-components": [
+        "warn",
+        { allowConstantExport: true },
+      ],
+      "react/prop-types": "off",
+    },
+    settings: {
+      react: {
+        version: "detect",
+      },
     },
   },
-};
+];
