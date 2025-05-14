75c0da887b513a9ea0cad5cdde471c93cd8bd436
tamir <tamirkhy@gmail.com>
2025-05-14 18:14:41 +0300

    Adjust root eslintconfig

--- eslint.config.js
+++ eslint.config.js
@@ -2,23 +2,29 @@ import eslintPlugin from "@typescript-eslint/eslint-plugin";
 import eslintParser from "@typescript-eslint/parser";
 import prettierConfig from "eslint-config-prettier";
 
-export default {
-  ignores: ["dist", "node_modules"],
-  files: ["**/*.ts", "**/*.tsx"],
-  languageOptions: {
-    ecmaVersion: "latest",
-    sourceType: "module",
-    parser: eslintParser,
+export default [
+  prettierConfig,
+  {
+    ignores: ["dist", "node_modules"],
+    files: ["**/*.ts", "**/*.tsx"],
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+      parser: eslintParser,
+      parserOptions: {
+        projectService: true,
+      },
+    },
+    plugins: {
+      "@typescript-eslint": eslintPlugin,
+    },
+    rules: {
+      ...eslintPlugin.configs.recommended.rules,
+      "@typescript-eslint/no-unused-vars": "warn",
+      "@typescript-eslint/no-explicit-any": "warn",
+      "@typescript-eslint/prefer-optional-chain": "warn",
+      "@typescript-eslint/explicit-function-return-type": "off",
+      "@typescript-eslint/explicit-module-boundary-types": "off",
+    },
   },
-  plugins: {
-    "@typescript-eslint": eslintPlugin,
-  },
-  extends: [eslintPlugin.configs.recommended, prettierConfig],
-  rules: {
-    "@typescript-eslint/no-unused-vars": "warn",
-    "@typescript-eslint/no-explicit-any": "warn",
-    "@typescript-eslint/prefer-optional-chain": "warn",
-    "@typescript-eslint/explicit-function-return-type": "off",
-    "@typescript-eslint/explicit-module-boundary-types": "off",
-  },
-};
+];
