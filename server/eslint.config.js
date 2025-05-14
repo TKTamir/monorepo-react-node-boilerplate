e8c910285af1d92dc4ffb246f8d38e2148d4fe1f
tamir <tamirkhy@gmail.com>
2025-05-14 18:14:51 +0300

    Adjust server eslintconfig

--- server/eslint.config.js
+++ server/eslint.config.js
@@ -1,14 +1,14 @@
 import baseConfig from "../eslint.config.js";
 import nodePlugin from "eslint-plugin-node";
 
-export default {
+export default [
   ...baseConfig,
-  plugins: {
-    ...baseConfig.plugins,
-    node: nodePlugin,
+  {
+    plugins: {
+      node: nodePlugin,
+    },
+    rules: {
+      "node/no-missing-import": "error",
+    },
   },
-  rules: {
-    "node/no-missing-import": "error",
-    "node/no-unsupported-features/es-syntax": "error",
-  },
-};
+];
